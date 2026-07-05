fa299c1d84a01854942efcf77cdf6b4ad6b6d589
Übermensch <149220325+Md-Inam@users.noreply.github.com>
2026-07-06 00:09:39 +0530

    ✅ Select By ID [db2] — approach1

---
+++ solutions/db2/easy/select-by-id/solution-approach1.sql
@@ -0,0 +1,19 @@
+-- ──────────────────────────────────────────────────
+-- Problem     Select By ID
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    db2
+-- Status      Accepted
+-- Submitted   2026-07-06, 12:09 a.m.
+-- ──────────────────────────────────────────────────
+
+
+/*
+    Enter your query here and follow these instructions:
+    1. Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+    2. The AS keyword causes errors, so follow this convention: "Select t.Field From table1 t" instead of "select t.Field From table1 AS t"
+    3. Type your code immediately after comment. Don't leave any blank line.
+*/
+SELECT * FROM CITY 
+WHERE ID=1661;
