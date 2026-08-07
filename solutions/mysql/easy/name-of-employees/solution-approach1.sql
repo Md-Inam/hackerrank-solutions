3bdb201cdb971a7a712189a9034b471072f83134
Übermensch <149220325+Md-Inam@users.noreply.github.com>
2026-08-07 16:38:04 +0530

    ✅ Employee Names [mysql] — approach1

---
+++ solutions/mysql/easy/name-of-employees/solution-approach1.sql
@@ -0,0 +1,17 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/name-of-employees/problem?isFullScreen=true
+-- Problem     Employee Names
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-08-07, 04:37 p.m.
+-- ──────────────────────────────────────────────────
+
+/*
+Enter your query here.
+*/
+SELECT NAME
+FROM EMPLOYEE
+ORDER BY NAME ASC
